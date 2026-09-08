f6239c7a25d8640b2f0da12c1ad66764dcc48379
JoaoPedro <90.joaopedrocarvalho@gmail.com>
2026-09-08 08:06:20 -0300

    Add files via upload

---
+++ src/main/java/org/example/model/Produto.java
@@ -0,0 +1,63 @@
+package org.example.model;
+
+public class Produto {
+    int id;
+    String nome;
+    double preco;
+    Categoria categoria;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Produto(int id, String nome, double preco, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Produto(String nome, double preco, Categoria categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", categoria=" + categoria +
+                '}';
+    }
+}
